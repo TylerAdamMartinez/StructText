fb3bade8617d56938803a2162403f76555ca21e6
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2026-04-28 20:03:10 -0500

    feat(Cargo): Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about)]
+struct Args {
+    values: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.values.is_empty() {
+        eprintln!("At least one argument is needed");
+        return;
+    }
+
+    for arg in args.values {
+        println!("{arg}");
+    }
 }
